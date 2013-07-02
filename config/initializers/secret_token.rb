d843e58a91965852e201ce462bd2cb5bc3f17dee
App Academy Student <student@appacademy.io>
2013-07-02 08:06:20 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Quora::Application.config.secret_token = '6c3e53211327cc6c9583d456d4c19decbfd01efadfa63363adf6ef0ac91a109966b5f1a2eb2cbec7706ae7856cc1632f879a6f4dbfdd14f7366442ef3a2b4b29'
